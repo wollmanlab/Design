022404c916b87b74d8a307579cb857ff132b1053
zehemminger <zachary.hemminger@gmail.com>
2025-06-23 23:33:55 -0700

    Update parameter variant list in create_parameter_file.py to include 'replicate_count' for enhanced testing flexibility
    - Removed previous dropout and noise parameters to streamline the configuration.
    - Added 'replicate_count' with a range of values to facilitate more comprehensive model testing.

--- Design/create_parameter_file.py
+++ Design/create_parameter_file.py
@@ -117,12 +117,10 @@ parameter_variant_list = [{'n_iters':[500]}]
 
 parameter_variant_list = [
     {
-        'P_drp_e':[0.0,0.05,0.1,0.25,0.5,1.0]
-    },
-    {
-        'P_noise_e':[0.0,0.05,0.1,0.25,0.5,1.0]
+        'replicate_count':[1,2,3,4,5,6,7,8,9,10]
     }
 ]
+
 
 total_combinations = []
 for parameter_variants in parameter_variant_list:
